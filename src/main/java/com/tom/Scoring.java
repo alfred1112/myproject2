65a5c848a3f6e70ee1b0c0de73b7bdcedc9f8fec
brian <10730319@dcsh.tp.edu.tw>
2022-03-04 16:08:48 +0800

    add Jack

---
+++ src/main/java/com/tom/Scoring.java
@@ -0,0 +1,10 @@
+package com.tom;
+
+public class Scoring {
+    public static void main(String[] args) {
+        student jack = new student();
+        jack.name = "jack";
+        jack.english = 70;
+        jack.math = 88;
+    }
+}
