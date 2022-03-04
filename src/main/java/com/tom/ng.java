1dbecb608b7897bebe71ce110065dca2b90c9d76
brian <10730319@dcsh.tp.edu.tw>
2022-03-04 15:24:29 +0800

    scoring NG

---
+++ src/main/java/com/tom/ng.java
@@ -0,0 +1,14 @@
+package com.tom;
+
+public class ng {
+    public static void main(String[] args) {
+        String [] names = {"Brian","Karen","Eenny","Benny","Peter"};
+        int [] english = {50,40,70,60,80};
+        int [] maths = {70,55,98,64,83};
+        for (int i = 0; i < 5; i++) {
+            System.out.println(names[i]+"\t"+maths[i]+"\t"+english[i]+"\t"+(maths[i]+english[i])/2);
+        }
+
+
+    }
+}
