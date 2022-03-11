7eb45b38d58954e985a83d11f9db4398be634ef0
3/11 <a43310175@gmail.com>
2022-03-11 14:49:30 +0800

    3/11，建構子

--- src/main/java/com/tom/Scoring.java
+++ src/main/java/com/tom/Scoring.java
@@ -2,9 +2,12 @@ package com.tom;
 
 public class Scoring {
     public static void main(String[] args) {
-        student jack = new student();
-        jack.name = "jack";
+        student jack = new student("jack");
+        //jack.name = "jack";
         jack.english = 70;
         jack.math = 88;
+        student hank = new student("hank",60,80);
+        jack.print();
+        hank.print();
     }
 }
--- src/main/java/com/tom/student.java
+++ src/main/java/com/tom/student.java
@@ -4,5 +4,19 @@ public class student {
     String name ;
     int english;
     int math;
+    public student(String name){
+        this.name = name;
+    }
+    public student(String name ,int english,int math){
+        this.name = name;
+        this.english = english;
+        this.math = math;
+    }
+    public student(){
+
+    }
+    public void print(){
+        System.out.println(name+"\t"+english+"\t"+math);
+    }
 
 }
