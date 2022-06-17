06e36ebdfd92a7cfb7139a02836df441b5415c17
alfred1112 <45417453+alfred1112@users.noreply.github.com>
2022-06-17 14:23:21 +0800

    0617

--- src/main/java/com/tom/score/Scoring.java
+++ src/main/java/com/tom/score/Scoring.java
@@ -11,19 +11,20 @@ public class Scoring {
         //jack.math = 88;
         student hank = new student("hank", 30, 80);
         GraduateStudent jane = new GraduateStudent("jane", 50, 80, 50);
-        List<student> students = new ArrayList<>();
-        students.add(jack);
-        students.add(jane);
-        students.add(hank);
-        for (int i = 0; i < students.size(); i++) {
-            student stv = students.get(i);
-            stv.print();
-        }
+        jack.print();
+        hank.print();
+        jane.print();
+//        List<student> students = new ArrayList<>();
+//        students.add(jack);
+//        students.add(jane);
+//        students.add(hank);
+//        for (int i = 0; i < students.size(); i++) {
+//            student stv = students.get(i);
+//            stv.print();
+//        }
     }
 
 //        jane.name ="jane";
-//        jack.print();
-//        hank.print();
-//        jane.print();
+
 //    }
 }
