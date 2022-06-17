71370ea9c2de4a6e4357666263e6a244d608183c
alfred1112 <45417453+alfred1112@users.noreply.github.com>
2022-06-17 16:28:10 +0800

    ScoringFile 2

--- src/main/java/com/tom/score/ScoringFile.java
+++ src/main/java/com/tom/score/ScoringFile.java
@@ -8,10 +8,20 @@ import java.io.IOException;
 public class ScoringFile {
     public static void main(String[] args) {
         try {
-            FileReader fileReader = new FileReader("score.txt");//throws
+            FileReader fileReader = new FileReader("scores.txt");//throws
             BufferedReader reader = new BufferedReader(fileReader);
+            //Jack,85,35
             String line = reader.readLine();
-            System.out.println(line);
+            while (line!=null){
+//                System.out.println(line);
+                String[] token = line.split(",");
+                String name = token[0];
+                int english =Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                student stu = new student(name, english ,math);
+                stu.print();
+                line = reader.readLine();
+            }
         }catch (FileNotFoundException e){
             System.out.println("檔案讀取失敗");
         }catch (IOException e){
